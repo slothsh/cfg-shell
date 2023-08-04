afe12a8c1496d371b8257d8537360e89c7d1b07c
Stefan Olivier <s.olivier1194@gmail.com>
2023-08-04 19:28:39 +0200

    added convenience aliases

--- zshrc.sh
+++ zshrc.sh
@@ -15,33 +15,10 @@ compinit
 
 # -------------------------------------------------
 
-# -- User Config --
+# -- User Environemnt --
+[ -f ~/.config/zsh/.zshenv ] && source ~/.config/zsh/.zshenv
 
-# Paths
-export EDITOR=nvim
-export HOST_NAME=$(uname -n | sed -e "s/ //g")
-export NVIM_CONFIG=~/.config/nvim
-export STARSHIP_CONFIG=~/.config/starship/starship.toml
-export TERMINAL_CONFIG=~/.config/alacritty/alacritty.yml
-export WALLPAPER=~/Pictures/geometric_01.jpg
-export WM_CONFIG=~/.config/qtile
-export DEV_PATH=~/dev
-export CFG_PATH="${DEV_PATH}/cfg"
-export PATH="$PATH:/home/slothsh/.local/bin"
-export LS_COLORS="$(vivid generate one-dark)"
-
-# vcpkg Paths
-export VCPKG_ROOT=/opt/vcpkg
-export VCPKG_DEFAULT_TRIPLET="x64-linux"
-export VCPKG_DEFAULT_HOST_TRIPLET="x64-linux"
-
-# Ranger config
-export RANGER_LOAD_DEFAULT_RC=TRUE
-
-# -- Helper functions
-source "${CFG_PATH}/cfg-shell/helpers.sh"
-
-# -- Aliases
+# -- Aliases --
 alias ll="ls -l --color=auto"
 alias lla="ls -la --color=auto"
 alias vv=nvim
@@ -50,8 +27,21 @@ alias tcfg="nvim $TERMINAL_CONFIG"
 alias pcfg="nvim ~/.zshrc"
 alias ccfg="nvim $STARSHIP_CONFIG"
 alias wcfg="nvim $WM_CONFIG"
+alias xcfg="nvim $TMUX_CONFIG"
+alias pcfg="nvim $PICOM_CONFIG"
+alias vw="nvim $WIKI_PATH"
 alias rr="ranger"
+alias cpwd="pwd | xclip -selection clipboard"
+alias xc="xclip -selection clipboard"
 alias luamake=/home/slothsh/.local/bin/lua-language-server/3rd/luamake/luamake
+alias zenv="nvim ~/.config/zsh/.zshenv"
+alias open="thunar"
+alias cat="bat --paging=never"
 
 # -- Starship Command Line --
 eval "$(starship init zsh)"
+
+# -- pyenv Config --
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
